a0e898a51b12114517b398ee6612000229f5afb9
Alex <alexxldub@gmail.com>
2019-11-01 15:44:08 +0200

    play with props

--- src/components/Profile/MyPosts/Post/Post.jsx
+++ src/components/Profile/MyPosts/Post/Post.jsx
@@ -2,11 +2,10 @@ import React from 'react';
 import s from './Post.module.css';
 
 const Post = (props) => {
-
   return (
     <div className={s.item}>
-      <img src='https://movies4maniacs.liberty.me/wp-content/uploads/sites/1218/2015/09/avatarsucks.jpg' />
-       <div>{ props.message }</div>
+      <img src='https://movies4maniacs.liberty.me/wp-content/uploads/sites/1218/2015/09/avatarsucks.jpg' alt='404' />
+        { props.message }
           <div>
         <span>like</span> { props.likesCount }
       </div>
