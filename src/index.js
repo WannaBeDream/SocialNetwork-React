81acd7db810a6349a9b90c0862f9287786f24a93
Alex <alexxldub@gmail.com>
2019-11-08 17:17:28 +0200

    store oop bind

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import * as serviceWorker from './serviceWorker';
-import state,{subscribe} from './myRedux/state'
+import store from './myRedux/state'
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import { BrowserRouter } from "react-router-dom";
-import { addPost, updateNewPostText } from './myRedux/state'
+
 
 
 // Карта вызовов    1) rerenderEntireTree(state) из index.js; 
@@ -18,13 +18,13 @@ let rerenderEntireTree = (state) => {
 
     ReactDOM.render(
         <BrowserRouter>
-            <App appState={state} addPost={addPost} updateNewPostText={updateNewPostText}/>
+            <App appState={state} addPost={store.addPost.bind(store)} updateNewPostText={store.updateNewPostText.bind(store)}/>
         </BrowserRouter>
         , document.getElementById('root'));
 }
-rerenderEntireTree(state); // первая отрисовка для взаимодействия чтобы после подключить observer 
+rerenderEntireTree(store.getState()); // первая отрисовка для взаимодействия чтобы после подключить observer 
 
-subscribe(rerenderEntireTree); // подписал наблюдателя ReactDom.render тем самым связав state и методы 
+store.subscribe(rerenderEntireTree); // подписал наблюдателя ReactDom.render тем самым связав state и методы 
 
 
 // If you want your app to work offline and load faster, you can change
--- src/myRedux/state.js
+++ src/myRedux/state.js
@@ -1,58 +1,59 @@
-let rerenderEntireTree = () => { // заменил эту функцию на функцию из index.js использую callbacks
+let store = {
+    _state : {
+        profilePage:{
+            posts : 
+            [
+            {id: 0, message: 'Hi, how are you?', likesCount: 12},
+            {id: 1, message: 'It\'s my first post', likesCount: 11},
+            {id: 2, message: 'Blabla', likesCount: 11},
+            {id: 3, message: 'Dada', likesCount: 11}
+            ],
+            newPostText:'ky ky'
+        },
+        dialogsPage:{
+            dialogs : [
+            {id: 1, name: 'Dimych'},
+            {id: 2, name: 'Andrew'},
+            {id: 3, name: 'Sveta'},
+            {id: 4, name: 'Sasha'},
+            {id: 5, name: 'Viktor'},
+            {id: 6, name: 'Valera'}
+            ],
+            messages : [
+                {id: 1, message: 'Hi'},
+                {id: 2, message: 'How is your it-kamasutra?'},
+                {id: 3, message: 'Yo'},
+                {id: 4, message: 'Yo'},
+                {id: 5, message: 'Yo'}
+            ],
+        },
+        sidebar:{
+           
+        },
+},
+    getState() {
+    return this._state
+    },
+    _callSubscriber() { // заменил эту функцию на функцию из index.js использую callbacks
     console.log('state was changed')
-}
-
-let state = {
-    profilePage:{
-        posts : 
-        [
-        {id: 0, message: 'Hi, how are you?', likesCount: 12},
-        {id: 1, message: 'It\'s my first post', likesCount: 11},
-        {id: 2, message: 'Blabla', likesCount: 11},
-        {id: 3, message: 'Dada', likesCount: 11}
-        ],
-        newPostText:'ky ky'
-    },
-    dialogsPage:{
-        dialogs : [
-        {id: 1, name: 'Dimych'},
-        {id: 2, name: 'Andrew'},
-        {id: 3, name: 'Sveta'},
-        {id: 4, name: 'Sasha'},
-        {id: 5, name: 'Viktor'},
-        {id: 6, name: 'Valera'}
-        ],
-        messages : [
-            {id: 1, message: 'Hi'},
-            {id: 2, message: 'How is your it-kamasutra?'},
-            {id: 3, message: 'Yo'},
-            {id: 4, message: 'Yo'},
-            {id: 5, message: 'Yo'}
-        ],
-    },
-    sidebar:{
-       
-    },
-}
-
-window.state = state //проверка  в браузере  BBL чтобі узнать где фиксируются данные, сейчас в state.profilePage.newPostText , но это не лучший пример т.к. используется круговая зависимость хоть и через render.js
-
-export const addPost = () => {
+},
+    addPost() {
     let newPost = {
-        id: state.profilePage.posts.length,
-        message: state.profilePage.newPostText,
+        id: this._state.profilePage.posts.length,
+        message: this._state.profilePage.newPostText,
         likesCount: 0,
     }
-    state.profilePage.posts.push(newPost);
-    rerenderEntireTree(state);
+    this._state.profilePage.posts.push(newPost);
+    this._callSubscriber(this._state);
+},
+    updateNewPostText(newText) {
+    this._state.profilePage.newPostText = newText;
+    this._callSubscriber(this._state);
+},
+    subscribe(observer) { // перезаписал функцию при вызове subscribe 
+    this._callSubscriber = observer;       // паттерн observer (addEventListner)
+}
 }
 
-export const updateNewPostText = (newText) => {
-    state.profilePage.newPostText = newText;
-    rerenderEntireTree(state);
-}
-
-export const subscribe = (observer) => { // перезаписал функцию при вызове subscribe 
-    rerenderEntireTree = observer;       // паттерн observer (addEventListner)
-}
-export default state;
+export default store;
+window.store = store //проверка  в браузере  BBL чтобі узнать где фиксируются данные, сейчас в state.profilePage.newPostText , но это не лучший пример т.к. используется круговая зависимость хоть и через render.js
