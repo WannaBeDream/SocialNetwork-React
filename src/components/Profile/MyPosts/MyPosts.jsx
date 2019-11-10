34cb2d6a57dcf20c47c09419c305e10395cecebb
Alex <alexxldub@gmail.com>
2019-11-10 20:06:11 +0200

    action creator action type

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -1,48 +1,51 @@
-import React from 'react';
-import s from './MyPosts.module.css';
-import Post from './Post/Post';
-
-const MyPosts = (props) => {
-
-    let newPostElement = React.createRef();
-
-    let postsElements = props.posts.map( p => <Post message={p.message} likesCount={p.likesCount}/>);
+import React from "react";
+import s from "./MyPosts.module.css";
+import Post from "./Post/Post";
+import {addPostActionCreator, updateNewPostTextActionCreator} from "../../../myRedux/state"
 
 
+const MyPosts = props => {
+  let newPostElement = React.createRef();
 
-    let addPost = () => {
-        // props.addPost(); // записываю значение из state.profilePage.newPostText в state.profilePage.posts
-        let newText= '';
-        let actionAdd = {type:'ADD-POST'};
-        let actionUpdate = {type:'UPDATE-NEW-POST-TEXT', newText };
-        props.dispatch(actionAdd);
-        props.dispatch(actionUpdate); // зануление (изменение в state.profilePage.newPostText)
-    }
+  let postsElements = props.posts.map(p => (
+    <Post message={p.message} likesCount={p.likesCount} />
+  ));
 
-    let onPostChange = () => {   // меняю state.profilePage.newPostText
-        let newText = newPostElement.current.value;
-        let action = {type:'UPDATE-NEW-POST-TEXT', newText };
-        props.dispatch(action);
-        // props.updateNewPostText(text); // state меняется при каждом нажатии и сохраняется в state.profilePage.newPostText
-        
-    }
+  let addPost = () => {
+    // props.addPost(); // записываю значение из state.profilePage.newPostText в state.profilePage.posts
+    // let newText= '';
+    // let actionAdd = {type:'ADD-POST'};
+    // let actionUpdate = {type:'UPDATE-NEW-POST-TEXT', newText };
+    props.dispatch(addPostActionCreator());
+    // props.dispatch(actionUpdate); // зануление (изменение в state.profilePage.newPostText)
+  };
 
-    return (
-        <div className={s.postsBlock}>
-            <h3>My posts</h3>
-            <div>
-                <div>
-                    <textarea onChange={onPostChange} ref={newPostElement} value={props.newPostText}></textarea>
-                </div>
-                <div>
-                    <button onClick={addPost}>Add post</button>
-                </div>
-            </div>
-            <div className={s.posts}>
-                { postsElements }
-            </div>
+  let onPostChange = () => {
+    // меняю state.profilePage.newPostText
+    let text = newPostElement.current.value;
+    let action = updateNewPostTextActionCreator(text);
+    props.dispatch(action);
+    // props.updateNewPostText(text); // state меняется при каждом нажатии и сохраняется в state.profilePage.newPostText
+  };
+
+  return (
+    <div className={s.postsBlock}>
+      <h3>My posts</h3>
+      <div>
+        <div>
+          <textarea
+            onChange={onPostChange}
+            ref={newPostElement}
+            value={props.newPostText}
+          ></textarea>
         </div>
-    )
-}
+        <div>
+          <button onClick={addPost}>Add post</button>
+        </div>
+      </div>
+      <div className={s.posts}>{postsElements}</div>
+    </div>
+  );
+};
 
 export default MyPosts;
--- src/myRedux/state.js
+++ src/myRedux/state.js
@@ -1,3 +1,6 @@
+const ADD_POST = "ADD-POST";
+const UPDATE_NEW_POST_TEXT = "UPDATE-NEW-POST-TEXT";
+
 let store = {
     _state: {
         profilePage: {
@@ -44,7 +47,7 @@ let store = {
     },
 
     dispatch(action) {   // { type: 'ADD-POST', message:'HEllo man!'}
-        if (action.type === 'ADD-POST') {
+        if (action.type === ADD_POST) {
             let newPost = {
                 id: this._state.profilePage.posts.length,
                 message: this._state.profilePage.newPostText,
@@ -52,12 +55,16 @@ let store = {
             }
             this._state.profilePage.posts.push(newPost);
             this._callSubscriber(this._state);
-        } else if (action.type === 'UPDATE-NEW-POST-TEXT') {
+        } else if (action.type === UPDATE_NEW_POST_TEXT) {
             this._state.profilePage.newPostText = action.newText;
             this._callSubscriber(this._state);
         }
     }
 }
+
+export const addPostActionCreator = () => ({ type: ADD_POST });
+
+export const updateNewPostTextActionCreator = (text) => ({ type: UPDATE_NEW_POST_TEXT, newText: text });
 
 export default store;
 window.store = store //проверка  в браузере  BBL чтобі узнать где фиксируются данные, сейчас в state.profilePage.newPostText , но это не лучший пример т.к. используется круговая зависимость хоть и через render.js
