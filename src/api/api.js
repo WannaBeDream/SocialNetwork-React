2b6e25fe2a06f74f56cb5f8ae33c1958eccd273f
Alex <alexxldub@gmail.com>
2019-12-07 23:15:26 +0200

    added DAL for Users

---
+++ src/api/api.js
@@ -0,0 +1,21 @@
+import * as axios from 'axios';
+
+
+
+const instance = axios.create({
+    withCredentials: true,
+    baseURL:'https://social-network.samuraijs.com/api/1.0/',
+    headers: {
+        "API-KEY": "6331f82f-4f98-4f15-b5e0-9b03f529a690"
+        }
+});
+
+
+export const usersAPI = {
+    getUsers (currentPage = 1, pageSize = 10) {
+        return  instance.get(`users?page=${currentPage}&count=${pageSize}`)
+          .then(response => {
+              return response.data;
+          });
+      }
+}
