c85e15a70a08512decbea1b4a6b8477d26c1641a
Alex <alexxldub@gmail.com>
2020-03-16 16:12:32 +0200

    completed paginator.jsx and paginator.test.jsx

--- src/components/common/Paginator/Paginator.jsx
+++ src/components/common/Paginator/Paginator.jsx
@@ -1,19 +1,31 @@
-import React from "react";
+import React, {useState} from "react";
 import styles from "./Paginator.module.css";
+import cn from "classnames";
 
-let Paginator = ({ totalUsersCount, pageSize, currentPage, onPageChanged }) => {
-  let pagesCount = Math.ceil(totalUsersCount / pageSize);
+let Paginator = ({ totalItemsCount, pageSize, currentPage, onPageChanged, portionSize = 10 }) => {
+  let pagesCount = Math.ceil(totalItemsCount / pageSize);
   let pages = [];
   for (let i = 1; i <= pagesCount; i++) {
     pages.push(i);
   }
 
+  let portionCount = Math.ceil(pagesCount / portionSize);
+  let [portionNumber, setPortionNumber] = useState(1);
+  let leftPortionPageNumber = (portionNumber - 1) * portionSize + 1;
+  let rightPortionPageNumber = portionNumber * portionSize;
+
   return (
-    <div>
-      {pages.map(page => {
+    <div className={styles.paginator}>
+      {portionNumber > 1 && 
+      <button onClick={() => {setPortionNumber(portionNumber - 1)}} >previus</button>  }
+
+      {pages.filter(p => p >= leftPortionPageNumber && p <= rightPortionPageNumber).map(page => {
         return (
           <span
-            className={currentPage === page && styles.selectedPage}
+            className={ cn({
+              [styles.selectedPage]: currentPage === page
+            }, styles.pageNumber)}
+            key={page}
             onClick={e => {
               onPageChanged(page);
             }}
@@ -22,6 +34,10 @@ let Paginator = ({ totalUsersCount, pageSize, currentPage, onPageChanged }) => {
           </span>
         );
       })}
+   {portionCount > portionNumber && 
+      <button onClick={() => {setPortionNumber(portionNumber + 1)}} >next</button>  }
+
+
     </div>
   );
 };
